0a4396f9e1386daaae6479689a55a49bba82ff22
jin1027 <rkzhs@naver.com>
2020-03-11 01:43:28 +0900

    당첨번호 크롤링 추가, 즐겨찾기 당첨 조회

--- lib/LotteryBall.dart
+++ lib/LotteryBall.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
 import 'state.dart';
 
 class LotteryBall{
-  Widget ticketSetPannel(TicketSet set, int valueKey)=>
-      Container(
-        child: ListView.separated(
-          physics: const BouncingScrollPhysics(),
-          itemCount: set.tickets.length,
-          itemBuilder: (context, index) => ListTile(
-            leading: Text("#${index+1}"),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: set.tickets[index].map((number) =>
-                  make(number),
-              )?.toList() ?? [],
-            ),
-          ),
-          separatorBuilder: (context, index) => Divider(),
-        ),
-        key: ValueKey<int>(valueKey),
-      );
-
   Widget make(int number) =>
       Stack(
         alignment: Alignment.center,
@@ -49,4 +33,130 @@ class LotteryBall{
           Text("$number", style:TextStyle(color: Colors.white)),
         ],
       );
+
+  Widget makeWithCheck(int number, LotterySet list) =>
+      list.numbers.contains(number)?
+          make(number):
+      Stack(
+        alignment: Alignment.center,
+        children:[
+          Container(
+            padding: EdgeInsets.all(18.0),
+            decoration: new BoxDecoration(
+              color: Colors.grey,
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: list.bonus == number ? Colors.purple:Colors.grey,
+                  blurRadius: 2.0,
+                  offset: Offset(
+                    1.0,
+                    2.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Text("$number", style:TextStyle(color: Colors.white)),
+        ],
+      );
+
+  Widget makeWinningNumber(LotterySet list){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: list.numbers.map((number) =>
+              LotteryBall().make(number),
+          )?.toList() ?? [],
+        ),
+        Icon(Icons.add),
+        LotteryBall().make(list.bonus)
+      ],
+    );
+  }
+}
+
+class LotterySet{
+  List<int> numbers;
+  int bonus;
+}
+
+class LotteryNumberLoader{
+  static List<LotterySet> list;
+  static int round = 0;
+  static int completed = 0;
+  static int range = 20;
+
+  static int getRound(){
+    return round;
+  }
+
+  static void _updateDay(){
+    DateTime time = DateTime.now();
+    DateTime from = DateTime(2020,3,7,8,39);
+    round = (time.millisecondsSinceEpoch - from.millisecondsSinceEpoch)~/(1000*60*60*24*7) + 901;
+  }
+
+  static int getLastRound(DateTime time){
+    _updateDay();
+    DateTime day = DateTime(time.year,time.month,time.day+7);
+    DateTime from = DateTime(2020,3,7,8,41);
+    int round = (day.millisecondsSinceEpoch - from.millisecondsSinceEpoch)~/(1000*60*60*24*7) + 901;
+    return round;
+  }
+
+  static void Load() async{
+    _updateDay();
+
+    list = List<LotterySet>(round+1);
+    completed = 0;
+
+    for(int i=round;i>=round-range;i--){
+      http.Response response = await http.get("https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo="+i.toString());
+      if(response.statusCode != 200){
+        round = 0;
+        return;
+      }
+      dom.Document document = parser.parse(response.body);
+      LotterySet balls = new LotterySet();
+      balls.numbers = new List<int>();
+
+      document.getElementsByClassName("num win").elementAt(0).getElementsByClassName("ball_645 lrg ball1").forEach((element) {
+        balls.numbers.add(int.parse(element.text));
+      });
+      document.getElementsByClassName("num win").elementAt(0).getElementsByClassName("ball_645 lrg ball2").forEach((element) {
+        balls.numbers.add(int.parse(element.text));
+      });
+      document.getElementsByClassName("num win").elementAt(0).getElementsByClassName("ball_645 lrg ball3").forEach((element) {
+        balls.numbers.add(int.parse(element.text));
+      });
+      document.getElementsByClassName("num win").elementAt(0).getElementsByClassName("ball_645 lrg ball4").forEach((element) {
+        balls.numbers.add(int.parse(element.text));
+      });
+      document.getElementsByClassName("num win").elementAt(0).getElementsByClassName("ball_645 lrg ball5").forEach((element) {
+        balls.numbers.add(int.parse(element.text));
+      });
+
+      document.getElementsByClassName("num bonus").elementAt(0).getElementsByClassName("ball_645 lrg ball1").forEach((element) {
+        balls.bonus = int.parse(element.text);
+      });
+      document.getElementsByClassName("num bonus").elementAt(0).getElementsByClassName("ball_645 lrg ball2").forEach((element) {
+        balls.bonus = int.parse(element.text);
+      });
+      document.getElementsByClassName("num bonus").elementAt(0).getElementsByClassName("ball_645 lrg ball3").forEach((element) {
+        balls.bonus = int.parse(element.text);
+      });
+      document.getElementsByClassName("num bonus").elementAt(0).getElementsByClassName("ball_645 lrg ball4").forEach((element) {
+        balls.bonus = int.parse(element.text);
+      });
+      document.getElementsByClassName("num bonus").elementAt(0).getElementsByClassName("ball_645 lrg ball5").forEach((element) {
+        balls.bonus = int.parse(element.text);
+      });
+
+      list[i] = balls;
+      completed++;
+    }
+  }
 }
--- lib/favorites.dart
+++ lib/favorites.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:intl/intl.dart';
+import 'LotteryBall.dart';
+import 'LotteryBall.dart';
+import 'LotteryBall.dart';
+import 'LotteryBall.dart';
 import 'state.dart';
-import 'lotto.dart';
 
 class Favorites extends StatefulWidget {
   @override
@@ -20,6 +23,8 @@ class _Favorites extends State<Favorites> with AutomaticKeepAliveClientMixin<Fav
   @override
   Widget build(BuildContext context) {
     super.build(context);
+    int round = LotteryNumberLoader.round;
+
     return ScopedModelDescendant<AppState>(
       builder: (context, child, model) =>
           ListView.separated(
@@ -37,12 +42,14 @@ class _Favorites extends State<Favorites> with AutomaticKeepAliveClientMixin<Fav
                             LotteryBall().make(number),
                         )?.toList() ?? [],
                       ),
+                      LotteryNumberLoader.getLastRound(model.favorites[index].createdAt)>round?
                       model.favorites[index].coverage5thPrize == null?
-                        SizedBox(
-                          width: 20, height: 20,
-                          child: CircularProgressIndicator(),
-                        )
-                        : Text("${(model.favorites[index].coverage5thPrize*100/8145060).toStringAsFixed(1)}%")
+                      SizedBox(
+                        width: 20, height: 20,
+                        child: CircularProgressIndicator(),
+                      )
+                          : Text("${(model.favorites[index].coverage5thPrize*100/8145060).toStringAsFixed(1)}%")
+                          :Text("몇등")
                     ],
                   ),
                   onTap: ()=>{
@@ -68,8 +75,57 @@ class LotteryList extends StatefulWidget {
 class _LotteryList extends State<LotteryList>{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body:LotteryBall().ticketSetPannel(widget.tickets, 0)
-    );
+    int last = LotteryNumberLoader.round;
+    int to = LotteryNumberLoader.getLastRound(widget.tickets.createdAt);
+
+    if(last < to || last - LotteryNumberLoader.range > to){
+      return Scaffold(
+          appBar: AppBar(
+              title:Text("목록")
+          ),
+          body:Container(
+            child: ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              itemCount: widget.tickets.tickets.length,
+              itemBuilder: (context, index) => ListTile(
+                leading: Text("#${index+1}"),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: widget.tickets.tickets[index].map((number) =>
+                      LotteryBall().make(number),
+                  )?.toList() ?? [],
+                ),
+              ),
+              separatorBuilder: (context, index) => Divider(),
+            ),
+            key: ValueKey<int>(0),
+          )
+      );
+    }
+    else{
+      LotterySet list = LotteryNumberLoader.list[to];
+      return Scaffold(
+          appBar: AppBar(
+            title:LotteryBall().makeWinningNumber(list)
+          ),
+          body:Container(
+            child: ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              itemCount: widget.tickets.tickets.length,
+              itemBuilder: (context, index) => ListTile(
+                leading: Text("#${index+1}"),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: widget.tickets.tickets[index].map((number) =>
+                      LotteryBall().makeWithCheck(number, list),
+                  )?.toList() ?? [],
+                ),
+              ),
+              separatorBuilder: (context, index) => Divider(),
+            ),
+            key: ValueKey<int>(0),
+          )
+      );
+    }
   }
 }
